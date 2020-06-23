9ad237f53db17ab991c015ca23bc817e3d38611f
박성범 <>
2020-06-23 16:20:44 +0900

    3

---
+++ src/chapter15/FourPointOperation.java
@@ -0,0 +1,20 @@
+package chapter15;
+
+public class FourPointOperation {
+    public static void main(String[]args) {
+        int a = 7;
+        int b = 3;
+
+        int add = a + b;
+        int sub = a - b;
+        int mult = a * b;
+        int div = a / b;
+        int mod = a % b;
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mult);
+        System.out.println(div);
+        System.out.println(mod);
+    }
+}
