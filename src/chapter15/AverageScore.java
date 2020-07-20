eccbcc15ff219d86df54bb936809e2c7b667b0eb
박성범 <shivash@naver.com>
2020-07-20 16:25:45 +0900

    13

---
+++ src/chapter15/AverageScore.java
@@ -0,0 +1,21 @@
+package chapter15;
+
+import java.util.Scanner;
+
+public class AverageScore {
+    public static void main(String[]args){
+        Scanner sc  = new Scanner(System.in);
+
+        int score = 0;
+        int sum = 0;
+
+        for(int i=1; i<=5; i++){
+            score = sc.nextInt();
+            if(score <40){
+                score = 40;
+            }
+            sum += score;
+        }
+        System.out.println(sum/5);
+    }
+}
