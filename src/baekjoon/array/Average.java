fa1d713a0d00cedf05b53a73fbecc12166d72dca
박성범 <shivash@naver.com>
2020-12-03 22:42:37 +0900

    24. 백준 8958번 0X 퀴즈

--- src/baekjoon/array/Average.java
+++ src/baekjoon/array/Average.java
@@ -30,7 +30,7 @@ import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
-
+        
         Scanner sc = new Scanner(System.in);
         double arr[] = new double[sc.nextInt()];
 
