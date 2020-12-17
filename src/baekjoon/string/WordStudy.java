5a6f827190ea2168f2e3b40e845eaed181670385
박성범 <shivash@naver.com>
2020-12-17 16:50:33 +0900

    32. 백준 자바 1157번 단어 공부

---
+++ src/baekjoon/string/WordStudy.java
@@ -0,0 +1,49 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+문제
+알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오.
+단, 대문자와 소문자를 구분하지 않는다.
+
+입력
+첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+
+출력
+첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+
+예제 입력           //      예제 출력
+Mississipi                  ?
+zZa                         Z
+ */
+public class WordStudy {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
+                arr[s.charAt(i) - 97]++;
+            } else {
+                arr[s.charAt(i) - 65]++;
+            }
+        }
+        int max = -1;
+        char ch = '?';
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65);
+            } else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.println(ch);
+    }
+}
