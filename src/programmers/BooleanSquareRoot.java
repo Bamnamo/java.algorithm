d1e83c6095efb62466d7caf6f44ce8f3037098bc
박성범 <shivash@naver.com>
2021-03-09 14:57:53 +0900

    49 프로그래머스 정수의 제곱근 판별, 자릿수 더하기

---
+++ src/programmers/BooleanSquareRoot.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class BooleanSquareRoot {
+    public long solution(long n) {
+        Double sqrt = Math.sqrt(n);
+
+        if (sqrt == sqrt.intValue()) {
+            return (long) Math.pow(sqrt + 1, 2);
+        } else return -1;
+    }
+}
