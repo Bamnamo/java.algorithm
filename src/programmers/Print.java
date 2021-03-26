bd8b66103706dad0118c56ffe710c6c2a5799548
박성범 <shivash@naver.com>
2021-03-26 13:40:13 +0900

    62 프로그래머스 프린터

---
+++ src/programmers/Print.java
@@ -0,0 +1,45 @@
+package programmers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Print {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<Printer> q = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            q.offer(new Printer(i, priorities[i]));
+        }
+
+        while (!q.isEmpty()) {
+
+            boolean flag = false;
+            int com = q.peek().prior;
+            for (Printer p : q) {
+                if (com < p.prior) {
+                    flag = true;
+                }
+            }
+
+            if (flag) {
+                q.offer(q.poll());
+            } else {
+                if (q.poll().location == location) {
+                    answer = priorities.length - q.size();
+                }
+            }
+        }
+        return answer;
+    }
+
+    class Printer {
+        int location;
+        int prior;
+
+        Printer(int location, int prior) {
+            this.location = location;
+            this.prior = prior;
+        }
+    }
+}
